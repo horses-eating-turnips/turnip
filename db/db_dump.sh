35bb331e59cb411d59cacafd7d6fd25879d0539e
Jonathan Hedstrom <jhedstrom@opensourcery.com>
2010-04-28 14:47:18 -0700

    Fixing sed command to replace all spaces.

--- db/db_dump.sh
+++ db/db_dump.sh
@@ -6,7 +6,7 @@ shopt -s expand_aliases
 if [ -z "$1" ]; then
   # get drupal's site name, then make lowercase and change spaces to dashes
   output="`drush --root=../drupal php-eval "print variable_get('site_name', 'local')"`.sql.gz"
-  output=`echo "$output" | sed -e 's/.*/\L&/' | sed -e 's/ /-/'`
+  output=`echo "$output" | sed -e 's/.*/\L&/' | sed -e 's/ /-/g'`
 else
   if [[ "$1" == "--release" ]]; then
     output="release-`date +%Y_%m_%d__%H_%M_%S`.sql.gz"
