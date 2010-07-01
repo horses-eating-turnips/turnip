f8c37a62bbe44f8dfbfa216dd5942e2b806a6068
Jonathan Hedstrom <jhedstrom@opensourcery.com>
2010-07-01 13:28:06 -0700

    Making mysqli the default connector.

--- drupal/sites/default/settings.php
+++ drupal/sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = 'mysqli://username:password@localhost/databasename';
 $db_prefix = '';
 
 /**
