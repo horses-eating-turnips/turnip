7167a97e4f1ab31a6261693b089824fc4b88fbfd
Jonathan Hedstrom <jhedstrom@gmail.com>
2010-09-29 10:41:08 -0700

    Run demo on pressflow.

--- drupal/sites/default/default.settings.php
+++ drupal/sites/default/default.settings.php
@@ -237,3 +237,21 @@ ini_set('url_rewriter.tags',        '');
 #   'forum'      => 'Discussion board',
 #   '@count min' => '@count minutes',
 # );
+
+/**
+ * Smart start:
+ *
+ * If you would prefer to be redirected to the installation system when a
+ * valid settings.php file is present but no tables are installed, remove
+ * the leading hash sign below.
+ *
+ * "Smart start" mode also assumes the site already has cron properly
+ * configured. So, it suppresses the cron notice until the normal warning
+ * threshold has elapsed since installation.
+ */
+# $conf['pressflow_smart_start'] = TRUE;
+
+$GLOBALS['simpletest_installed'] = TRUE;
+if (preg_match("/^simpletest\d+$/", $_SERVER['HTTP_USER_AGENT'])) {
+  $db_prefix = $_SERVER['HTTP_USER_AGENT'];
+}
