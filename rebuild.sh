079f119162237bc2ae750f3fb0fbea30431b985f
Jonathan Hedstrom <jhedstrom@gmail.com>
2010-10-13 16:24:16 -0700

    Merging rebuild.sh from 6.x.

---
+++ rebuild.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# @@ Replace os_project with the actual project name, then remove this line @@
+#
+# Rebuild os_project in the drupal directory.
+#
+# To use this command you must have `drush make`, `cvs` and `git` installed.
+#
+
+PROFILE="os_project"
+
+# Generate a complete Drupal build.
+echo "Building PROJECT..."
+
+MAKE=$(cat $PROFILE/$PROFILE.make - <<EOF
+api = "2"\n
+core = "7.x"\n
+projects[drupal][type] = "core"
+projects[drupal][version] = "7.0-beta1"\n
+EOF
+)
+
+# remove old directories
+rm -rf drupal
+rm -rf $PROFILE/modules/contrib
+rm -rf $PROFILE/modules/stock
+rm -rf $PROFILE/themes/stock
+rm -rf $PROFILE/libraries
+
+# Link profiler into the custom profile library.
+mkdir $PROFILE/libraries
+cd $PROFILE/libraries
+ln -s ../../drupal/profiles/opensourcery_install/libraries/profiler profiler
+cd -
+
+# Build the drupal directory from the make file.
+echo -e "$MAKE" | drush make --yes --contrib-destination=profiles/$PROFILE - drupal
+
+# Link settings.php and files into sites/default
+cd drupal/sites/default
+ln -s ../../../shared/settings.php settings.php
+ln -s ../../../shared/files files
+cd - 
+
+# Clean-up the profile directory, and sym-link it into the drupal directory.
+rsync -az drupal/profiles/$PROFILE/ $PROFILE/
+rm -rf drupal/profiles/$PROFILE
+cd drupal/profiles
+ln -s ../../$PROFILE $PROFILE
+cd -
