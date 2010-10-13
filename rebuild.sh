a247a486216a300921e15f6229ddfb02292b9f20
Jonathan Hedstrom <jhedstrom@gmail.com>
2010-10-13 16:08:24 -0700

    Tweak to rebuild.sh.

--- rebuild.sh
+++ rebuild.sh
@@ -25,19 +25,16 @@ rm -rf $PROFILE/modules/contrib
 rm -rf $PROFILE/modules/stock
 rm -rf $PROFILE/themes/stock
 echo -e "$MAKE" | drush make --yes --contrib-destination=profiles/$PROFILE - drupal
-exit
-
-
-# since profile is sym-linked, move modules and themes in there
-mv drupal/profiles/opensourcery_install/modules/contrib drupal/profiles/$PROFILE/modules
-
-
-mv drupal/sites/all/modules/contrib drupal/profiles/$PROFILE/modules/contrib
-mv drupal/sites/all/modules/stock drupal/profiles/$PROFILE/modules/stock
-mv drupal/sites/all/themes/stock drupal/profiles/$PROFILE/themes/stock
-rm -rf drupal/sites/all/modules
-rm -rf drupal/sites/all/themes
 
 # settings and files
-ln -s shared/settings.php drupal/sites/default/settings.php
-ln -s shared/files drupal/sites/default/files
+cd drupal/sites/default
+ln -s ../../../shared/settings.php settings.php
+ln -s ../../../shared/files files
+cd -ls 
+
+# os_project directory
+rsync -az drupal/profiles/$PROFILE/ $PROFILE/
+rm -rf drupal/profiles/$PROFILE
+cd drupal/profiles
+ln -s ../../$PROFILE $PROFILE
+cd -
