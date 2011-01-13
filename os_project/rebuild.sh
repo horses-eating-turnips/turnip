8db594c6c8b57d841078be9b19ce812798369423
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-01-13 11:01:07 -0800

    Remove os_project's copy of rebuild.sh.

--- os_project/rebuild.sh
+++
@@ -1,4 +0,0 @@
-# To run this script, you'll need drush_make, git, and cvs installed.
-#
-# To build with working CVS repositories, add the --working-copy flag to the drush make command (and make sure you have CVSROOT set).
-drush make --no-core --contrib-destination=. os_project.make
