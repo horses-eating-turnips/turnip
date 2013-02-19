55af4c249cf725a0492d338f8e1f9581978bc70a
Jonathan Hedstrom <jhedstrom@gmail.com>
2013-02-19 10:25:08 -0800

    Copying in Vagrant manifests and scripts.

---
+++ Vagrantfile
@@ -0,0 +1,28 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# See http://kvz.io/blog/2013/01/16/vagrant-tip-keep-virtualbox-guest-additions-in-sync/
+# Vagrant's autoloading may not have kicked in
+require 'vagrant-vbguest' unless defined? VagrantVbguest::Config
+#VagrantVbguest::Config.auto_update = false
+
+Vagrant::Config.run do |config|
+  config.vm.box = "precise32"
+
+  # The url from where the 'config.vm.box' box will be fetched if it
+  # doesn't already exist on the user's system.
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+
+  # Boot with a GUI so you can see the screen. (Default is headless)
+  # config.vm.boot_mode = :gui
+
+  # Forward a port from the guest to the host, which allows for outside
+  # computers to access the VM, whereas host only networking does not.
+  config.vm.forward_port 80, 8888
+
+  # Enable provisioning with Puppet.
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = "manifests"
+    puppet.manifest_file  = "webserver.pp"
+  end
+end
---
+++ manifests/webserver.pp
@@ -0,0 +1,62 @@
+exec { "apt-get update":
+    command => "/usr/bin/apt-get update",
+}
+
+Package {
+    require => Exec["apt-get update"],
+}
+
+package { "apache2": }
+package { "apache2.2-common": }
+package { "libapache2-mod-php5": }
+package { "php5": }
+package { "php5-cli": }
+package { "php-apc": }
+package { "php5-mysql": }
+package { "php5-gd": }
+package { "php-pear": }
+package { "mysql-server": }
+
+service { "apache2":
+    ensure => "running",
+    enable => "true",
+    require => Package["apache2"],
+}
+
+service { "mysql":
+    ensure => "running",
+    enable => "true",
+    require => Package["mysql-server"],
+}
+
+file { "/etc/apache2/sites-available/default":
+    notify => Service["apache2"],
+    ensure => "present",
+    source => "/vagrant/manifests/default",
+    require => Package["apache2"],
+}
+
+exec { "mysql password":
+    unless => "/usr/bin/mysql -uroot -ppassword",
+    command => "/usr/bin/mysqladmin -u root password password",
+    notify => Service["mysql"],
+    require => Package["mysql-server"],
+}
+
+exec { "mod rewrite":
+    notify => Service["apache2"],
+    command => "/usr/sbin/a2enmod rewrite",
+    require => Package["apache2"],
+}
+
+exec { "create-drupal-db":
+    unless => "/usr/bin/mysql -udrupal drupal",
+    command => "/usr/bin/mysql -uroot -ppassword -e \"create database drupal; grant all on drupal.* to drupal@localhost;\"",
+    require => Exec["mysql password"],
+}
+
+exec { "install-drush":
+    unless => "/usr/bin/which drush",
+    command => "/usr/bin/pear channel-discover pear.drush.org; /usr/bin/pear upgrade-all; /usr/bin/pear install drush/drush; /usr/bin/drush -h",
+    require => Package["php-pear"],
+}
